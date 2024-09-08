986c9fd970feb958e07dd76ca9a719329c575813
Trưởng Dương <truongduong@Truongs-Mac-mini.local>
2024-09-08 22:39:28 +0700

    add alert util

---
+++ src/utils/alert.ts
@@ -0,0 +1,27 @@
+import { Alert } from "react-native";
+
+export const createTwoButtonAlert = (title: string, message: string) => {
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+}
+
+export const createThreeButtonAlert = () => {
+    Alert.alert('Alert Title', 'My Alert Msg', [
+      {
+        text: 'Ask me later',
+        onPress: () => console.log('Ask me later pressed'),
+      },
+      {
+        text: 'Cancel',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {text: 'OK', onPress: () => console.log('OK Pressed')},
+    ]);
+}
